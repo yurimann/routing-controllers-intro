ad801f210a2e83f288c219a898b40db146c7cc0d
Yuri <yuri.manahan@gmail.com>
2017-01-30 14:16:08 -0500

    Completed exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get '/' => 'pages#welcome'
+root 'pages#welcome'
 get '/about' => 'pages#about'
 get '/contest' => 'pages#contest'
-get '/kitten/:size' => 'pages#kitten'
-get '/kittens/:size' => 'pages#kittens'
-get '/secrets/:magic_word' => 'pages#secrets'
+get '/kitten/:size' => 'pages#kitten', as: 'kitten'
+get '/kittens/:size' => 'pages#kittens', as: 'kittens'
+get '/secrets/:magic_word' => 'pages#secrets', as: 'secret'
 end
