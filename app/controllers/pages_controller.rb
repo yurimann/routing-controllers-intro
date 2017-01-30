36e5f61985b680f8e351fad1541f63286ca07c66
Yuri <yuri.manahan@gmail.com>
2017-01-30 14:09:39 -0500

    Conditional page up and running

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,34 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
 
-def welcome
-end
+  def welcome
+    @header = "This is the welcome page"
+  end
 
-def about
-end
+  def about
+    @header = "About this site"
+  end
 
-def contest
-end
+  def contest
+    flash[:notice] = "Sorry, the contest has ended. You didn't win."
+    redirect_to "/"
+  end
 
+  def kitten
+  end
+
+  def kittens
+  end
+
+  def secrets
+    if params[:magic_word] != "test"
+      flash[:alert] = "Sorry wrong passcode"
+      redirect_to '/'
+    end
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 get '/' => 'pages#welcome'
 get '/about' => 'pages#about'
 get '/contest' => 'pages#contest'
-
+get '/kitten/:size' => 'pages#kitten'
+get '/kittens/:size' => 'pages#kittens'
+get '/secrets/:magic_word' => 'pages#secrets'
 end
